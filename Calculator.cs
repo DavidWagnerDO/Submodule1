2cca7ff202dca0dbcbe2bc0863fe6e3bf59189b1
David Wagner <daw@dataone.de>
2022-05-30 17:01:50 +0200

    format

--- Calculator.cs
+++ Calculator.cs
@@ -3,12 +3,13 @@
 namespace Submodule1;
 public static class Calculator1
 {
- public static int Add(int summand1, int summand2){
-     return summand1 + summand2;
- }
+    public static int Add(int summand1, int summand2)
+    {
+        return summand1 + summand2;
+    }
 
-public static int Subtract(int minuend, int subtrahend ){
-    return Calculator2.Subtract(minuend, subtrahend);
-}
-
+    public static int Subtract(int minuend, int subtrahend)
+    {
+        return Calculator2.Subtract(minuend, subtrahend);
+    }
 }
